50aff078238b949e4b723c7d851b4e2f0b86d950
royi.cohen <royi.cohen@au10tix.com>
2023-06-18 11:50:16 +0300

    load react native webview from text input

--- screen/webview/securemewebview.js
+++ screen/webview/securemewebview.js
@@ -1,65 +1,61 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Linking, StyleSheet, Platform, View, TextInput} from "react-native";
-import { WebView } from 'react-native-webview';
-import Permissions from './Permissions';
+import React, { useState } from 'react';
+import { View, StatusBar, StyleSheet, TextInput } from 'react-native';
+import WebView from 'react-native-webview';
 
+const App = () => {
+  const [url, setUrl] = useState('');
+  const [editedUrl, setEditedUrl] = useState(url);
 
-export default function App() {
-  const useMount = func => useEffect(() => func(), []);
-  
-  
-  const useInitialURL = () => {
-    const [url, setUrl] = useState(null);
-    const [processing, setProcessing] = useState(true);
-    // Asking for android permissions
-    if(Platform.OS === 'android'){
-      Permissions.apply
+  const handleUrlSubmit = () => {
+    if (editedUrl.trim() !== '') {
+      setUrl(editedUrl.trim());
     }
-    useMount(() => {
-      const getUrlAsync = async () => {
-        // Get the deep link used to open the app
-        const initialUrl = await Linking.getInitialURL();
-        setUrl(initialUrl);
-        
-        // setUrl('https://stg.10tix.me/65rV93Ka8MzG9Xpr4GKs');
-        setProcessing(false);
-      };
-      getUrlAsync();
-    });
-    return { url, processing };
   };
-  const { url: initialUrl, processing } = useInitialURL();
-  
+
   function onMessage(data) {
     const dataArray = JSON.parse(data.nativeEvent.data);
-    if(dataArray.payload.value === "/success") {
+    if (dataArray.payload.value === "/success") {
       alert("The session completed successfully");
-    } 
+    }
   }
-  
+
   return (
-    <View>
-    <TextInput
-    multiline={false}
-    placeholder="URL"
-    onSubmitEditing={()=>{
-      setUrl=value
-    }}     
-    />
-    <WebView source={{ uri: "about:blank" }}
-    geolocationEnabled={true}
-    mediaPlaybackRequiresUserAction={false}
-    javaScriptEnabled={true}
-    useWebKit={true}
-    allowsInlineMediaPlayback={true}
-    javaScriptEnabledAndroid={true}
-    onMessage={onMessage}
-    />
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={editedUrl}
+        onChangeText={setEditedUrl}
+        placeholder="Enter URL"
+        onSubmitEditing={handleUrlSubmit}
+      />
+      <WebView
+        source={{ uri: url }}
+        geolocationEnabled={true}
+        mediaPlaybackRequiresUserAction={false}
+        javaScriptEnabled={true}
+        useWebKit={true}
+        allowsInlineMediaPlayback={true}
+        javaScriptEnabledAndroid={true}
+        onMessage={onMessage}
+      />
     </View>
-    );
-  }
-  
-  
-  const styles = StyleSheet.create({});
-  
-  
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    margin: 10,
+    padding: 5,
+  },
+});
+
+export default App;
